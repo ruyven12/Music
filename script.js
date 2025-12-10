9d90a7b9453d1b9c1043ca247dec44a30fa68edd
ruyven12 <ruyven12@gmail.com>
2025-12-10 14:24:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1438,7 +1438,7 @@ async function loadAndShowAlbumPhotos(
       const gridEl = document.createElement("div")
       gridEl.className = "photo-grid"
       gridEl.style.display = "grid"
-      gridEl.style.gridTemplateColumns = "repeat(6, 200px)"
+      gridEl.style.gridTemplateColumns = "repeat(4, 200px)"
       gridEl.style.gap = "12px"
       photosWrap.appendChild(gridEl)
 
